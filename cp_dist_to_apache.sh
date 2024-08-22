e501c28d70fa7405de705c03f52d63168e3cce98
HolgDer <h.schernewski@variobotic.de>
2024-08-22 09:55:18 +0200

    updating to generalize all user and change systemctl to service command to match also docker container specification

--- cp_dist_to_apache.sh
+++ cp_dist_to_apache.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 sudo rm -rf /var/www/varioboticOS/dist
-sudo cp -r ~/tmp/dist /var/www/varioboticOS/
+sudo cp -r /home/$USER/tmp/dist /var/www/varioboticOS/
 sudo rm -rf /var/www/varioboticOS/html_old
 sudo mv /var/www/varioboticOS/html /var/www/varioboticOS/html_old
 sudo mv /var/www/varioboticOS/dist /var/www/varioboticOS/html
 
 sudo rm -rf /dist
 
-sudo systemctl restart apache2.service
+sudo service apache2 restart
